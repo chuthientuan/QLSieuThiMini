e05f71fc6cfd174eb54dab47e8b98e082dcf0489
Loz <tuanchu24112004@gmail.com>
2024-10-20 10:20:54 +0700

    Tao lop ket noi database

---
+++ Classes/DataBaseProcess.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSieuThiMini.Classes
+{
+    internal class DataBaseProcess
+    {
+        private string strConnect;
+        private SqlConnection sqlConnect = null;
+
+        // Constructor để đọc chuỗi kết nối từ appsettings.json
+        public DataBaseProcess()
+        {
+            // Thiết lập cấu hình để đọc từ appsettings.json
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            strConnect = configuration.GetConnectionString("DefaultConnection");
+        }
+        //Phương thức mở kết nối
+        void OpenConnect()
+        {
+            sqlConnect = new SqlConnection(strConnect);
+            if (sqlConnect.State != ConnectionState.Open)
+                sqlConnect.Open();
+        }
+        //Phương thức đóng kết nối
+        void CloseConnect()
+        {
+            if (sqlConnect.State != ConnectionState.Closed)
+            {
+                sqlConnect.Close();
+                sqlConnect.Dispose();
+            }
+        }
+        //Phương thức thực thi câu lệnh Select trả về một DataTable
+        public DataTable DataReader(string sqlSelct)
+        {
+            DataTable tblData = new DataTable();
+            OpenConnect();
+            SqlDataAdapter sqlData = new SqlDataAdapter(sqlSelct, sqlConnect);
+            sqlData.Fill(tblData);
+            CloseConnect();
+            return tblData;
+        }
+        //Phương thức thực hiện câu lệnh dạng insert, update, delete
+        public void DataChange(string sql)
+        {
+            OpenConnect();
+            SqlCommand sqlcomma = new SqlCommand();
+            sqlcomma.Connection = sqlConnect;
+            sqlcomma.CommandText = sql;
+            sqlcomma.ExecuteNonQuery();
+            CloseConnect();
+        }
+    }
+}
