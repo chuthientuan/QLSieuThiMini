06133995e04efd0fdae6ae4ec79c4e0cb42635f8
Tuan <tuanchu24112004@gmail.com>
2024-11-05 21:35:02 +0700

    Add form HDB

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace QLSieuThiMini
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmHDB());
         }
     }
 }
