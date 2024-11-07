fc2655609fe8d9ee4b1a5a76584e1763d6c609f3
hinzz14 <taoeopiet2004@gmail.com>
2024-11-07 20:01:33 +0700

    def

--- frmHDN.Designer.cs
+++ frmHDN.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
-            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.cbbTKMHD = new Guna.UI2.WinForms.Guna2ComboBox();
-            this.btnTimKiem = new Guna.UI2.WinForms.Guna2Button();
             this.guna2GroupBox2 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.btnTimKiem = new Guna.UI2.WinForms.Guna2Button();
+            this.cbbTKMHD = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2GroupBox3 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2GroupBox4 = new Guna.UI2.WinForms.Guna2GroupBox();
             this.guna2Panel1.SuspendLayout();
             this.guna2GroupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -48,6 +51,17 @@
             this.guna2Panel1.Size = new System.Drawing.Size(200, 673);
             this.guna2Panel1.TabIndex = 0;
             // 
+            // guna2GroupBox2
+            // 
+            this.guna2GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2GroupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2GroupBox2.ForeColor = System.Drawing.Color.Black;
+            this.guna2GroupBox2.Location = new System.Drawing.Point(0, 163);
+            this.guna2GroupBox2.Name = "guna2GroupBox2";
+            this.guna2GroupBox2.Size = new System.Drawing.Size(200, 510);
+            this.guna2GroupBox2.TabIndex = 1;
+            this.guna2GroupBox2.Text = "Chức năng";
+            // 
             // guna2GroupBox1
             // 
             this.guna2GroupBox1.Controls.Add(this.btnTimKiem);
@@ -61,32 +75,6 @@
             this.guna2GroupBox1.Size = new System.Drawing.Size(200, 163);
             this.guna2GroupBox1.TabIndex = 0;
             this.guna2GroupBox1.Text = "Tìm kiếm";
-            // 
-            // guna2HtmlLabel1
-            // 
-            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(5, 45);
-            this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(5);
-            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(83, 22);
-            this.guna2HtmlLabel1.TabIndex = 0;
-            this.guna2HtmlLabel1.Text = "Mã hóa đơn";
-            // 
-            // cbbTKMHD
-            // 
-            this.cbbTKMHD.BackColor = System.Drawing.Color.Transparent;
-            this.cbbTKMHD.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cbbTKMHD.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbbTKMHD.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cbbTKMHD.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cbbTKMHD.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.cbbTKMHD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
-            this.cbbTKMHD.ItemHeight = 30;
-            this.cbbTKMHD.Location = new System.Drawing.Point(5, 66);
-            this.cbbTKMHD.Name = "cbbTKMHD";
-            this.cbbTKMHD.Size = new System.Drawing.Size(183, 36);
-            this.cbbTKMHD.TabIndex = 1;
             // 
             // btnTimKiem
             // 
@@ -104,22 +92,71 @@
             this.btnTimKiem.TabIndex = 2;
             this.btnTimKiem.Text = "Tìm kiếm";
             // 
-            // guna2GroupBox2
+            // cbbTKMHD
             // 
-            this.guna2GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.guna2GroupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2GroupBox2.ForeColor = System.Drawing.Color.Black;
-            this.guna2GroupBox2.Location = new System.Drawing.Point(0, 163);
-            this.guna2GroupBox2.Name = "guna2GroupBox2";
-            this.guna2GroupBox2.Size = new System.Drawing.Size(200, 510);
-            this.guna2GroupBox2.TabIndex = 1;
-            this.guna2GroupBox2.Text = "Chức năng";
+            this.cbbTKMHD.BackColor = System.Drawing.Color.Transparent;
+            this.cbbTKMHD.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbbTKMHD.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbTKMHD.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbbTKMHD.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbbTKMHD.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbbTKMHD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbbTKMHD.ItemHeight = 30;
+            this.cbbTKMHD.Location = new System.Drawing.Point(5, 66);
+            this.cbbTKMHD.Name = "cbbTKMHD";
+            this.cbbTKMHD.Size = new System.Drawing.Size(183, 36);
+            this.cbbTKMHD.TabIndex = 1;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(5, 45);
+            this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(5);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(83, 22);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Mã hóa đơn";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.White;
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(1062, 39);
+            this.guna2Panel2.TabIndex = 1;
+            // 
+            // guna2GroupBox3
+            // 
+            this.guna2GroupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2GroupBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2GroupBox3.ForeColor = System.Drawing.Color.Black;
+            this.guna2GroupBox3.Location = new System.Drawing.Point(0, 39);
+            this.guna2GroupBox3.Name = "guna2GroupBox3";
+            this.guna2GroupBox3.Size = new System.Drawing.Size(1062, 327);
+            this.guna2GroupBox3.TabIndex = 2;
+            this.guna2GroupBox3.Text = "Thông tin chung";
+            // 
+            // guna2GroupBox4
+            // 
+            this.guna2GroupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2GroupBox4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2GroupBox4.ForeColor = System.Drawing.Color.Black;
+            this.guna2GroupBox4.Location = new System.Drawing.Point(0, 366);
+            this.guna2GroupBox4.Name = "guna2GroupBox4";
+            this.guna2GroupBox4.Size = new System.Drawing.Size(1062, 307);
+            this.guna2GroupBox4.TabIndex = 3;
+            this.guna2GroupBox4.Text = "Thông tin mặt hàng";
             // 
             // frmHDN
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.guna2GroupBox4);
+            this.Controls.Add(this.guna2GroupBox3);
+            this.Controls.Add(this.guna2Panel2);
             this.Controls.Add(this.guna2Panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -143,5 +180,8 @@
         private Guna.UI2.WinForms.Guna2ComboBox cbbTKMHD;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
         private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox3;
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox4;
     }
 }
