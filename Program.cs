ff187ed542ab7a6ce576a8ba98d6eca77c852e16
k59ta <k59ta@TAY_ANH>
2024-11-08 15:27:58 +0700

    a

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace QLSieuThiMini
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmNhanVien());
+
         }
     }
 }
