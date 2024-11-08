50b219792c10e790ce3d4f28e392f921e5bfdbb9
hinzz14 <taoeopiet2004@gmail.com>
2024-11-08 23:32:18 +0700

    sql

--- Classes/DataBaseProcess.cs
+++ Classes/DataBaseProcess.cs
@@ -12,9 +12,9 @@ namespace QLSieuThiMini.Classes
 {
     internal class DataBaseProcess
     {
-        string strConnect = "Data Source=DESKTOP-15605MG\\SQLEXPRESS;" +
+        string strConnect = "Data Source=LOU1S\\SQLEXPRESS;" +
                 "DataBase=QuanlySieuthi;User ID=sa;" +
-                "Password=abc123;Integrated Security=false";
+                "Password=abc123;Integrated Security=true";
         SqlConnection sqlConnect = null;
 
         //Phương thức mở kết nối
