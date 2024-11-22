7d8076a1f591497c4f65648906d5238bf1457fad
k59ta <k59ta@TAY_ANH>
2024-11-23 00:00:19 +0700

    aaa

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace QLSieuThiMini
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmLogin());
+            Application.Run(new FrmAdmin());
         }
     }
 }
